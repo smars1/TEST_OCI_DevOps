e91ca97734b0a679e75f84964193c1e1ec78c51c
DAPC98 <diego198mayo@gmail.com>
2025-03-22 13:02:52 -0700

    Template: managing terraform since Oci env

---
+++ oci_devops/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "oci" {
+    bucket         = "terraform-state-bucket"
+    namespace      = "axztjiaddc9d"   # namespace: desde OCI Storage
+    region         = "us-phoenix-1"   # Ajusta segun tu region
+    key            = "terraform.tfstate"  # Nombre del archivo state
+    auth           = "InstancePrincipal"  # O usa API Key si ejecutas localmente
+  }
+}
